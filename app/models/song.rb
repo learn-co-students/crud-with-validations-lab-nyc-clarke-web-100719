08a4aa0e15668bdf6d73298a1c6c9edf69652d6e
DannyBrito <britodanny1997@gmail.com>
2019-11-05 20:29:41 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ApplicationRecord
     validates :title, presence: true
     validates :title, uniqueness:{scope: [:release_year, :artist_name]}
+    validates :artist_name, presence: true
     validates :released, inclusion: { in: [true, false] }
     with_options if: :released? do |song|
         song.validates :release_year, presence: true
